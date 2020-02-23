92d1e9b570e0992a7b03d64990b6d47fb411acd1
JuanEspinosa <juancamilo100@hotmail.com>
2020-02-22 23:14:49 -0500

    Logging

--- app/config.js
+++ app/config.js
@@ -9,7 +9,7 @@ export const routePath = {
 // tslint:disable-next-line:no-console
 console.log("base URL:");
 // tslint:disable-next-line:no-console
-console.log(process.env.TIME_TRACKER_API_BASE_URL);
+console.log(process.env);
 
 export const TIME_TRACKER_API_BASE_URL = process.env.TIME_TRACKER_API_BASE_URL;
 export const TIME_TRACKER_API_PORT = process.env.TIME_TRACKER_API_PORT;
